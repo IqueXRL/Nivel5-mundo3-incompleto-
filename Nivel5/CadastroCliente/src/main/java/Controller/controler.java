458cf87d877e4e9099aa7eace517ef15843dffa6
Caique Thomaz <126598893+IqueXRL@users.noreply.github.com>
2023-11-16 23:31:24 -0300

    Add files via upload

---
+++ Nivel5/CadastroCliente/src/main/java/Controller/controler.java
@@ -0,0 +1,24 @@
+package Controller;
+
+public class UserController {
+
+    // Método para cadastrar um novo usuário
+    public void cadastrarUsuario(String nome, String email, String senha) {
+        System.out.println("Novo usuário cadastrado:");
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("Senha: " + senha);
+    }
+
+    public void buscarUsuarioPorId(int userId) {
+        System.out.println("Buscando usuário com ID: " + userId);
+    }
+
+
+    public static void main(String[] args) {
+        // Exemplo de utilização do controlador
+        UserController controller = new UserController();
+        controller.cadastrarUsuario("Exemplo", "exemplo@email.com", "senha123");
+        controller.buscarUsuarioPorId(1);
+    }
+}
